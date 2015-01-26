ae93f215fb27acfec3883a2262c46fec5f4aed1a
Pawel Chojnacki <pawel@chojnacki.ws>
2015-01-26 14:51:40 +0100

    fix runtime errors

--- src/main/groovy/com/wikia/gradle/MarathonConnector.groovy
+++ src/main/groovy/com/wikia/gradle/MarathonConnector.groovy
@@ -18,7 +18,7 @@ class MarathonConnector {
     def postConfig(String marathonURL, String app_id, String appRequestBody, boolean forceNeedsNewVersion = false) {
         try {
             def currentCfg = this.client(marathonURL).get(path: "/v2/apps/${app_id}") {}
-            logger.debug(currentCfg)
+            logger.debug(currentCfg.toString())
         } catch (HttpResponseException ex) {
             if (ex.statusCode != 404) {
                 throw ex
@@ -26,7 +26,7 @@ class MarathonConnector {
         }
 
         logger.debug("Post to marathon")
-        logger.debug(appRequestBody)
-        logger.debug(this.client(marathonURL).post(path: "/v2/apps", body: appRequestBody, requestContentType: ContentType.JSON))
+        logger.debug(appRequestBody.toString())
+        logger.debug(this.client(marathonURL).post(path: "/v2/apps", body: appRequestBody, requestContentType: ContentType.JSON).toString())
     }
 }
