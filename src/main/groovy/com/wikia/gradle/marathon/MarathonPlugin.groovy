2a52c0b3fa5b58b82ccb5d0c02e29360e5342e5d
Pawel Chojnacki <pawel@chojnacki.ws>
2015-02-20 17:42:09 +0100

    uploaded ArtifactLocator

--- src/main/groovy/com/wikia/gradle/marathon/MarathonPlugin.groovy
+++ src/main/groovy/com/wikia/gradle/marathon/MarathonPlugin.groovy
@@ -1,5 +1,6 @@
 package com.wikia.gradle.marathon
 
+import com.wikia.gradle.marathon.common.StageCreator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,5 +9,9 @@ class MarathonPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.ext.Marathon = MarathonTask.class
+        project.extensions.create("marathon", StageCreator)
+//        project.task('productionDeploy'). << {
+//            project.marathon.
     }
+
 }
