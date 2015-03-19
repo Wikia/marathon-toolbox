96afe282e0463f6529021c47b1cd31e2d046a7fd
Mateusz Hołowiecki <Mateusz.Holowiecki@wikia-inc.com>
2015-03-19 15:52:27 +0100

    add confirmation task to deploy and setup task

--- src/main/groovy/com/wikia/gradle/marathon/MarathonPlugin.groovy
+++ src/main/groovy/com/wikia/gradle/marathon/MarathonPlugin.groovy
@@ -8,12 +8,20 @@ import org.gradle.api.Project
 
 class MarathonPlugin implements Plugin<Project> {
 
+    private static final String IS_CONFIRMATION_NOT_NEEDED = "marathon.isConfirmationNotNeeded"
+    private static final String deployConfirmation = "deployConfirmation"
+
     @Override
     void apply(Project project) {
         project.ext.Marathon = MarathonTask.class
         project.extensions.create("deployments", MarathonExtension)
+        def isConfirmationNotNeeded = project.hasProperty(IS_CONFIRMATION_NOT_NEEDED).
+                toString().toBoolean()
 
         project.afterEvaluate { evaluatedProject ->
+            if (!isConfirmationNotNeeded) {
+                evaluatedProject.task(deployConfirmation, type: ConfirmationTask) {}
+            }
             def deployments = evaluatedProject.extensions.
                     getByName("deployments") as MarathonExtension
             deployments.getStageNames().each { stageName ->
@@ -21,16 +29,21 @@ class MarathonPlugin implements Plugin<Project> {
                     group = "Marathon Setup"
                     description = "Setup configured stage: ${stageName}"
                     stage = deployments.getStage(stageName).validate()
+                    if (!isConfirmationNotNeeded) {
+                        dependsOn(deployConfirmation);
+                    }
                 }
 
                 if (deployments.getStage(stageName).resolve(App).isMaven()) {
-                    evaluatedProject.
-                            task("deploy${stageName.capitalize()}", type: MarathonTask) {
-                                group = "Marathon Deployment"
-                                description = "Deploy configured stage: ${stageName}"
-                                stage = deployments.getStage(stageName).validate()
-                                dependsOn(stage.resolve(App).mavenPublishTaskName)
-                            }
+                    evaluatedProject.task("deploy${stageName.capitalize()}", type: MarathonTask) {
+                        group = "Marathon Deployment"
+                        description = "Deploy configured stage: ${stageName}"
+                        stage = deployments.getStage(stageName).validate()
+                        dependsOn(stage.resolve(App).mavenPublishTaskName)
+                        if (!isConfirmationNotNeeded) {
+                            dependsOn(deployConfirmation);
+                        }
+                    }
                 }
             }
         }
