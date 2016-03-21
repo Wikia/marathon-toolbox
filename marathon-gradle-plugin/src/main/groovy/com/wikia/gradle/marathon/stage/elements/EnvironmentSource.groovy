f3c5b3d5ca1e7602fa80d6118e281158dca96c03
Pawel Chojnacki <pawel@chojnacki.ws>
2016-03-21 01:51:40 +0100

    environmentSource use proper list

--- marathon-gradle-plugin/src/main/groovy/com/wikia/gradle/marathon/stage/elements/EnvironmentSource.groovy
+++ marathon-gradle-plugin/src/main/groovy/com/wikia/gradle/marathon/stage/elements/EnvironmentSource.groovy
@@ -1,18 +1,16 @@
 package com.wikia.gradle.marathon.stage.elements
 
 import com.wikia.gradle.marathon.common.Validating
-import mesosphere.marathon.client.model.v2.HealthCheck
-
-import static com.wikia.gradle.marathon.common.ConfigResolver.dslToParamClosure
 
 class EnvironmentSource implements Validating {
-    List<Closure<HealthCheck>> healthcheckList = new LinkedList<>()
 
-    public def fromConsulKV(String val){
+    List<Closure<String>> consulPrefixes = new LinkedList<>()
 
+    public def fromConsulKV(GString val) {
+        consulPrefixes.add { val }
     }
 
     public def fromConsulKV(Closure closure) {
-        healthcheckList.add(dslToParamClosure(closure))
+        consulPrefixes.add(closure)
     }
 }
